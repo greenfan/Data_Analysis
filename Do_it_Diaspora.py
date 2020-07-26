5e0037affdf4376c53da05f26305c467129f295f
greenfan <rdwyer@fastmail.com>
2020-07-27 00:02:46 +0200

    Commit - version .3
    Removed unnecessary code and added comments.

--- Do_it_Diaspora.py
+++ Do_it_Diaspora.py
@@ -2,18 +2,14 @@
 import feedparser
 from datetime import datetime
 from selenium import webdriver
+import sys
 import time
 import re
+import random
 
 from selenium.webdriver.common.keys import Keys
-
-
-
 url = "https://zapier.com/engine/rss/7955983/OpenScienceRSS"
 NewsFeed = feedparser.parse(url)
-
-
-
 # Configure our web browser
 options = webdriver.ChromeOptions()
 #options.add_argument('headless');
@@ -23,12 +19,6 @@ options.binary_location = "/usr/bin/chromium"
 driver = webdriver.Chrome(options=options)
 # End web browser config
 
-#Headless instatiation
-
-
-
-
-
 driver.get('https://diasp.org/stream')
 
 time.sleep(3)
@@ -36,40 +26,51 @@ time.sleep(3)
 ### Begin Actions
 username = driver.find_element_by_css_selector("#user_username")
 password = driver.find_element_by_css_selector("#user_password")
-username.send_keys("helloWorld" + Keys.TAB )
-password.send_keys("wouldn'tyoulinke2know" + Keys.ENTER)
+username.send_keys("opensciencedaily" + Keys.TAB )
+password.send_keys("Zxcvfgfg21" + Keys.ENTER)
+#password.send_keys(u'\ue007')
 
 driver.get('https://diasp.org/stream')
-
 time.sleep(15)
-
-
 textbox = driver.find_element_by_css_selector("#status_message_text")
-
 todays_date = format(datetime.today().day).zfill(2)
-print(todays_date)
+print("today's date is: {}.".format(todays_date))
 ### Begin action
 
 
 
-a_count = len(NewsFeed.entries)
-print(a_count)
-t_count = 0
-print("T count {}".format(t_count))
-for i in range(0, a_count):
-    entry = NewsFeed.entries[i]
-    pdate = entry.published.split()
-    if todays_date in pdate[1]:
-        t_count = t_count + 1
-        print(entry.published)
+
+#Begin parsing the RSS Feed
 
 
+
+#
+# 1. Determine how many stories we will post
+
+argcount = len(sys.argv)
+print("argcount is {}".format(argcount))
+
+if argcount == 2:
+    t_count = int(sys.argv[1])    
+else:
+    t_count = 0
+    print("T count {}".format(t_count))
+    # Count manually to find out how many ### to post  t_count === number of stories to post
+    for i in range(0, 9):
+        # DECALRE entry
+        entry = NewsFeed.entries[i]
+        pdate = entry.published.split()
+        # Add time divergence
+        if todays_date in pdate[1]:
+            t_count = t_count + 1
+        print(entry.title)
 
 print("total_new_stories from {} is {}".format(todays_date,t_count))
 
 
 
-for i in reversed(range(0, t_count)):
+for i in range(0, t_count):
+    #DECALRE entry
     entry = NewsFeed.entries[i]
     pdate = entry.published
     textbox.send_keys('#### {0}\n****\n'.format((entry.title)))
@@ -79,11 +80,18 @@ for i in reversed(range(0, t_count)):
         hashtags = entry.author
         hashtags2 = re.sub("[ ]", ",", hashtags)
         hashlist = re.split(',', hashtags2)
+        hashlist = hashlist[0:9]
+
     except AttributeError:
-	hashlist = [ "insert_hashtags" ]
+        itemlist = ["solar", "energy", "news", "renewable", "PV", "hydrogen", "solarpower", "sustainability", "photovoltaic", "green", "renewableenergy", "futurology", "RSS"]
+        hashlist = random.choices(itemlist, k=5)
+
 
     hash = ['#' + i for i in hashlist]
-    stop_words = ["#&", "#and", "#", "#with", "#the"]
+
+#REmove unwanted hashtags
+    stop_words = ["#&", "#and", "#a", "#", "#with", "#the", "#for", "#company", "#of"]
+#Finally declare hashtags as fullhash
     fullhash = list(set(filter(lambda w: w not in stop_words, hash)))
     print(*fullhash)
     textbox.send_keys('> {0}\n {1} \n {2} \n ****'.format(entry.summary,entry.link,', '.join(fullhash)))
@@ -91,15 +99,14 @@ for i in reversed(range(0, t_count)):
     submitbutton = driver.find_element_by_css_selector("#submit").click()
     time.sleep(1)
     driver.find_element_by_xpath('//*[@id="submit"]').click()
-    time.sleep(900)
+    if i == t_count:
+        latesturl.write(entry.title)
+    time.sleep(1337)
 
 
 
 driver.close()
 print("...")
 time.sleep(1)
+latesturl.close()
 print("I have typed it and clicked submit, Master.")
-
-
-
-
